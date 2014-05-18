dc5707c2cdadae0d6b868f0cf6d57f9c78fff6ca
V David Zvenyach <vdavez@gmail.com>
2014-05-17 21:44:13 -0400

    first pass at running the parser

--- parse/parse.js
+++ parse/parse.js
@@ -3,30 +3,31 @@ var path = require("path")
 var rr = require('readdir-recursive');
 var _ = require("underscore");
 var et = require('elementtree');
+var beautify_html = require('js-beautify').html;
 
 function run () {
-	rr.file("code-test", function(file) {
+	//Open the directory and, for each file, parse the reg
+	rr.file("code-text", function(file) {
 		readReg(file, function () {
 		});
 	})
 } 
 
-function test () {
-	readReg("./code-test/1/1-1/1-102.txt", function () {
+/*function test () {
+	readReg("./code-test/1/1-31/1-3199.txt", function () {
 		console.log("Done!")
 	})
-}
+}*/
 
-//Open the directory
 
-//For each file, read the text and fire off the parser
 function readReg (f, callback) {
+	//For the file, read the text and fire off the parser
 	var fname = path.basename(f, '.txt')
 	var out = fs.readFileSync(f, 'utf-8');
 	// Combine the lines together
 	combineLines(out, function (r) {
 		buildRegTree(r, fname, function (rt) {
-			fs.writeFileSync("./code-xml/" + fname + ".xml", rt, {encoding:"utf8"})
+			fs.writeFileSync("./code-xml/" + fname + ".xml", beautify_html(rt, {indent_size: 2}), {encoding:"utf8"})
 			callback();
 		})
 	})
@@ -61,6 +62,10 @@ function buildRegTree (r, name, callback) {
 	var root = element("level");
 	subElement(root, "type").text = "section";
 	subElement(root, "num").text = name;
+	var anno = et.Element("level");
+	var type = et.Element("type");
+	type.text = "annotations";
+	anno.append(type)
 
 	//Now we traverse the tree
 	var lines = r.split(/\n/)
@@ -74,32 +79,50 @@ function buildRegTree (r, name, callback) {
 			// This is a subsection
 			var level = subElement(root, "level");
 			var num = et.Element("num")
-			num.text = line.match(/^(\d+\.\d+\.)/)[0]
+			num.text = line.match(/^(\d+\.\d+(\.)?)/)[0]
 			level.append(num)
 			var text = et.Element("text");
 			text.text = line.replace(/^(\d+\.\d+\.)/,"");
 			level.append(text);
 		} else if (line.match(/^\(\w\).*/g)) {
-			// This is a paragraph 
+			// This is a paragraph or a subparagraph
+			// TODO: Differentiate between the two...
 			var sublevel = et.Element("level");
 			var num = et.Element("num")
 			num.text = line.match(/^\(\w\)/)[0]
-			console.log(line.match(/^\(\w\)/))
 			sublevel.append(num)
 			var text = et.Element("text");
 			text.text = line.replace(/^\(\w\)/,"");
 			sublevel.append(text);
 			level = root.getchildren()[root.getchildren().length-1];
 			level.append(sublevel)
+		} else if (line.match(/^[A-Z]+\:/) || line.match(/^[A-Z]{2}.*\:/)) {
+			// Annotation
+			var note = et.Element("level")
+			var head = et.Element("heading")
+			head.text = line.match(/^[A-Z].*\:/)[0].replace("\:","")
+			note.append(head);
+			var text = et.Element("text");
+			text.text = line.replace(/[A-Z].*\:/,"");
+			note.append(text);
+			anno.append(note);
+		} else if (line == "") {
+			//Skip the line
+		} else if (line.match(/^.*/)) {
+			var level = root.getchildren()[root.getchildren().length-1]
+			var text = et.Element("text")
+			text.text = line
+			level.append(text)
+			root.insert(root.getchildren().length-1, level)
 		} else {
-			// TODO: Add subparagraphs and errors
-			console.log("None!")
+			//Uncaught
+			console.log(name + ": line " + index + " -- " + line);
 		}
 	})
-
+	root.append(anno)
 	etree = new ElementTree(root);
 	xml = etree.write({'xml_declaration': false});
 	callback(xml)
 }
 
-test()
+run()
